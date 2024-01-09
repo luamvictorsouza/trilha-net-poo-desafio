7a875e7f192f7b6e7f3f902a2c8b30b374da4aad
luamvictorsouza <142535018+luamvictorsouza@users.noreply.github.com>
2024-01-09 14:00:26 -0300

    Update Nokia.cs

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -6,3 +6,49 @@ namespace DesafioPOO.Models
         // TODO: Sobrescrever o método "InstalarAplicativo"
     }
 }
+
+namespace DesafioPOO.Models
+{
+    
+    public abstract class Smartphone
+    {
+        public string Numero { get; set; }
+        protected string Modelo { get; set; }
+        protected string IMEI { get; set; }
+        protected int Memoria { get; set; }
+
+        public abstract void InstalarAplicativo(string nome);
+    }
+
+    public class Nokia : Smartphone
+    {
+        public Nokia(string numero, string modelo, string imei, int memoria)
+        {
+            Numero = numero;
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
+        }
+        
+        public override void InstalarAplicativo(string nome)
+        {
+            Console.WriteLine($"Instalando {nome} no Nokia {Modelo}");
+        }
+    }
+
+    public class iPhone : Smartphone
+    {
+        public iPhone(string numero, string modelo, string imei, int memoria)
+        {
+            Numero = numero;
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
+        }
+
+        public override void InstalarAplicativo(string nome)
+        {
+            Console.WriteLine($"Instalando {nome} no iPhone {Modelo}");
+        }
+    }
+}
